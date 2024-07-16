d7cb001de0a57d514a414a5ba89d6cf855470737
Hibi_10000 <87810571+Hibi-10000@users.noreply.github.com>
2024-07-16 22:52:34 +0900

    Change Chunk Builder to stop just deleting configuration items

--- src/main/java/com/limeshulkerbox/bsvsb/mixin/MixinVideoOptionsScreen.java
+++ src/main/java/com/limeshulkerbox/bsvsb/mixin/MixinVideoOptionsScreen.java
@@ -7,28 +7,42 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
+import net.minecraft.client.gui.widget.OptionListWidget;
 import net.minecraft.client.option.GameOptions;
-import net.minecraft.client.option.SimpleOption;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
-import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VideoOptionsScreen.class)
 public abstract class MixinVideoOptionsScreen extends GameOptionsScreen {
+    @Shadow
+    private OptionListWidget list;
+
     public MixinVideoOptionsScreen(Screen parent, GameOptions gameOptions, Text title) {
         super(parent, gameOptions, title);
     }
 
-    @Inject(method = "getOptions", at = @At("RETURN"), cancellable = true)
-    private static void removeChunkBuilderButton(GameOptions gameOptions, CallbackInfoReturnable<SimpleOption<?>[]> cir) {
-        SimpleOption<?>[] value = cir.getReturnValue();
-        value = ArrayUtils.removeElement(value, gameOptions.getChunkBuilderMode());
-        cir.setReturnValue(value);
+    @Inject(method = "init", at = @At("TAIL"))
+    protected void disableButton(CallbackInfo ci) {
+        // https://github.com/CaffeineMC/sodium-fabric/pull/964#issuecomment-966529568
+        ClickableWidget chunkBuilderMode = this.list.getWidgetFor(gameOptions.getChunkBuilderMode());
+        if (chunkBuilderMode != null) {
+            chunkBuilderMode.active = false;
+            chunkBuilderMode.setMessage(
+                Text.translatable(
+                    "options.generic_value",
+                    Text.translatable("options.prioritizeChunkUpdates"),
+                    Text.literal("Sodium")
+                )
+            );
+            chunkBuilderMode.setTooltip(null);
+        }
     }
 
     @Override
